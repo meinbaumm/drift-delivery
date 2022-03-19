2250a4a4d9255a632449ac5f97844cc399e7e853
meinbaumm <max413111@mail.ru>
2022-03-19 16:49:35 +0300

    remove assets folder

--- config/dev.exs
+++ config/dev.exs
@@ -20,15 +20,7 @@ config :drift_delivery, DriftDeliveryWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: []
 
 # ## SSL Support
 #
